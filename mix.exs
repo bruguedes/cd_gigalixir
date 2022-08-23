6a66598208df231c83f1a3767c31c6985a8baef7
bruguedes <bruguedes@gmail.com>
2022-08-22 22:36:34 -0500

    fix: mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CdGigalixir.MixProject do
 
   def project do
     [
-      app: :pipeline_ci,
+      app: :cd_gigalixir,
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
